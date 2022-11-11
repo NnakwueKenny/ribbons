9f81d5d55fd76026696148e7508080a4e7cf7e9d
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-11-11 15:19:41 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter} from "react-router-dom";
 
+document.tidioChatLang = document.querySelector('html').getAttribute('lang');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
