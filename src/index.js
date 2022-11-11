4841c0e9fe849c3062984d9ff3a213a3516e81dc
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-11-11 17:50:17 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter} from "react-router-dom";
 
-document.tidioChatLang = navigator.language.slice(0,2);
-console.log(document.tidioChatLang);
-console.log(document.querySelector('html').getAttribute('lang'))
+// document.tidioChatLang = navigator.language.slice(0,2);
+// console.log(document.tidioChatLang);
+// console.log(document.querySelector('html').getAttribute('lang'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
