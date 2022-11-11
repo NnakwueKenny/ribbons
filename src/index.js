87af8f093daba67c306512e30c763098d2e67e78
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-11-11 17:57:16 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <div id="google_translate_element"><App /></div>
 		</BrowserRouter>
   </React.StrictMode>
 );
