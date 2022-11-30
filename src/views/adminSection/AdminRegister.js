69e8d2268b98677d645aa24c6d30a03c7392429c
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-11-30 13:51:25 +0100

    update

--- src/views/adminSection/AdminRegister.js
+++ src/views/adminSection/AdminRegister.js
@@ -53,7 +53,7 @@ const AdminLogin = () => {
           toggleMessageContent(setregisterMessage, regData);
           setRegisterStatus(false);
         } else {
-          fetch('https://ribbons.onrender.com/admin/register',
+          fetch('https://ribbons.onrender.com/admin/registe',
             {
               method: 'post',
               headers: {
