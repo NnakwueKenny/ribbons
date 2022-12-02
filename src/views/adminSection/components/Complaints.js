421221b0a51ea01f17bfa99b8d20e072378cf0ca
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-12-02 17:06:42 +0100

    update

--- src/views/adminSection/components/Complaints.js
+++ src/views/adminSection/components/Complaints.js
@@ -265,13 +265,13 @@ const Complaints = () => {
                 </div>
                 <div className={`relative flex flex-col lg:flex-row gap-4 md:gap-2 py-4 w-full max-w-6xl overflow-hidden ${showTopNav? 'h-16 lg:h-auto': ''}`}>
                     <FormControl >
-                        <InputLabel id="demo-simple-select-helper-label">{`${currentCategory === ''? 'Status': currentCategory === 'open'? 'Open': 'Closed'}`}</InputLabel>
+                        <InputLabel id="demo-simple-select-helper-label">{`${currentCategory === 'open'? 'Open': currentCategory === 'closed'? 'Closed': 'Status'}`}</InputLabel>
                         <Select
                             labelId="demo-simple-select-helper-label"
                             id="demo-simple-select-helper"
                             value={''}
                             label="Status"
-                            onChange={(e) => {e.target.value === ''? alert('Nothing is happening here'): setComplaintCategory(complaintCategories[`${e.target.value}`]); setCurrentCategory(e.value)}}
+                            onChange={(e) => {e.target.value === ''? alert('Nothing is happening here'): setComplaintCategory(complaintCategories[`${e.target.value}`]); setCurrentCategory(e.target.value)}}
                             className='w-full lg:w-24 h-12'
                             >
                             <MenuItem value={''}>None</MenuItem>
